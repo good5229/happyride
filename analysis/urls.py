8e336b26099c76944c2d5157fea266dcd38a67c2
김종백 <kjb45841497@gmail.com>
2019-08-13 12:32:57 +0900

    feat(introduce): create introduce html page

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from analysis.views import Select_Location
+from analysis.views import Select_Location, Introduce
 
 app_name = 'analysis'
 
 urlpatterns = [
+    path('introduce/', Introduce.as_view(), name='intro'),
     path('select_location/', Select_Location.as_view(), name='select_loc'),
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -2,5 +2,10 @@
 from django.views.generic import TemplateView
 
 
+class Introduce(TemplateView):
+    template_name = 'analysis/introduce.html'
+
 class Select_Location(TemplateView):
     template_name = 'analysis/select_location.html'
+
+
